335415677b74a571a85c42b7f81a0022dc5b91b9
Maclean, James <James.Maclean@fil.com>
2017-02-10 08:55:20 +0000

    update

---
+++ animation-styleguide/custom.js
@@ -0,0 +1,316 @@
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeIn-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeIn-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeIn fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+ } );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeInScaleUp-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInScaleUp-target' ).each( function() {
+
+         $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-fadeInScaleUp fil-ani-is-hidden' );
+
+    } );
+
+    e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeInScaleDown-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInScaleDown-target' ).each( function() {
+
+         $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-fadeInScaleDown fil-ani-is-hidden' );
+
+    } );
+
+    e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeInUp-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInUp-target' ).each( function() {
+
+         $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInUp fil-ani-is-hidden' );
+
+    } );
+
+    e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeInDown-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInDown-target' ).each( function() {
+
+         $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInDown fil-ani-is-hidden' );
+
+    } );
+
+    e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeInLeft-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeInLeft-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInLeft fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+ } );
+
+
+
+ $( document ).on( 'click', '.js-fil-ani-fadeInRight-trigger', function( e ) {
+
+      $( '.js-fil-ani-fadeInRight-target' ).each( function() {
+
+           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInRight fil-ani-is-hidden' );
+
+      } );
+
+      e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-FlipUp90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipUp90-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipUp90  fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-FlipDown90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipDown90-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipDown90  fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeOut-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeOut-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOut fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+ } );
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeOutScaleDown-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutScaleDown-target' ).each( function() {
+
+       $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutScaleDown fil-ani-is-hidden' );
+
+  } );
+
+  e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeOutScaleUp-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutScaleUp-target' ).each( function() {
+
+       $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutScaleUp fil-ani-is-hidden' );
+
+  } );
+
+  e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeOutUp-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutUp-target' ).each( function() {
+
+       $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutUp fil-ani-is-hidden' );
+
+  } );
+
+  e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeOutDown-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutDown-target' ).each( function() {
+
+       $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutDown fil-ani-is-hidden' );
+
+  } );
+
+  e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeOutLeft-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeOutLeft-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutLeft fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-fadeOutRight-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeOutRight-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutRight fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-FlipOutUp90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipOutUp90-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipOutUp90 fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-FlipOutDown90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipOutDown90-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipOutDown90 fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-FlipLeft180-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipLeft180-target' ).each( function() {
+
+         if ( $( this ).hasClass( 'fil-ani-card__flipped' ) ) {
+              $( this ).removeClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180 fil-ani-card__flipped' );
+              $( this ).addClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipRight180' );
+         } else {
+             $( this ).removeClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipRight180' );
+             $( this ).addClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180 fil-ani-card__flipped' );
+         }
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-PulseScale-trigger', function( e ) {
+
+     $( '.js-fil-ani-PulseScale-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-1000 fil-ani-PulseScale' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+
+$( document ).on( 'click', '.js-fil-ani-PulseOutline-trigger', function( e ) {
+
+     $( '.js-fil-ani-PulseOutline-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-PulseOutline' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-Shake-trigger', function( e ) {
+
+     $( '.js-fil-ani-Shake-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-1200 fil-ani-Shake' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-fil-ani-Spotlight-trigger', function( e ) {
+
+     $( '.js-fil-ani-Spotlight-target' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-Spotlight' );
+          $( '.js-overlay-target' ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-400 fil-ani-fadeIn' );
+
+     } );
+
+     e.preventDefault();
+
+} );
